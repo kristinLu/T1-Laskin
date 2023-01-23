c92eb543264dc6689069d5bac606fc990076604c
Kristin Luik <kristin.luik@outlook.com>
2023-01-23 22:30:51 +0200

    Tehtävä 1

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [luku1, asetaLuku1] = useState(0);
+  const [luku2, asetaLuku2] = useState(0);
+  const [tulos, laskeTulos] = useState('');
+
+  const naytaSumma = () => laskeTulos(parseInt(luku1) + parseInt(luku2));
+  const naytaErotus = () => laskeTulos(parseInt(luku1) - parseInt(luku2));
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Result: {tulos}</Text>
+      <TextInput style={styles.input} keyboardType='numeric' onChangeText={luku1 => asetaLuku1(luku1)} value={luku1} />
+      <TextInput style={styles.input} keyboardType='numeric' onChangeText={luku2 => asetaLuku2(luku2)} value={luku2} />
+      <View style={styles.buttons}>
+        <View style={styles.button}>
+          <Button onPress={naytaSumma} title="+" />
+        </View>
+        <View style={styles.button}>
+          <Button onPress={naytaErotus} title="-" />
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +34,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  input: {
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  button: {
+    width: 30,
+    margin: 10,
   },
 });
